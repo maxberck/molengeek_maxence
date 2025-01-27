ac2875859800a5ee14082289257f926924b5e0a6
maxence <maxence@MacBook-Air-de-Virginie.local>
2025-01-27 10:51:15 +0100

    ajout de quelques comentaires

--- Accueil/public/js/main.js
+++ Accueil/public/js/main.js
@@ -1,10 +1,13 @@
 //menu burger ( frite )
 document.addEventListener('DOMContentLoaded', () => {
+    // aller chercher les éléments dans html
     const burgerMenu = document.querySelector('.burger-menu');
     const menu = document.querySelector('ul');
 
     burgerMenu.addEventListener('click', () => {
+        // creation de la classe open avec un toggle pour pouvoir allumer fermer
         burgerMenu.classList.toggle('open');
+        // vérifier si la class open est active ou pas
         if (menu.classList.contains('open')) {
             menu.style.opacity = '0';
             menu.style.visibility = 'hidden';
@@ -21,14 +24,17 @@ document.addEventListener('DOMContentLoaded', () => {
 // carousel
 let carouDivs = document.querySelectorAll('.carousel');
 let header = document.querySelector('header');
+// creation d'objet
  images = {
     carou1: "url('./public/img/img-carousel.jpeg')",
     carou2: "url('./public/img/img-carousel1.jpg')",
     carou3: "url('./public/img/carou1.jpg')"
 };
+ // si il y a un header
 if (header) {
+    // forEach parce qu'il faut lire les élément de l'objet
     carouDivs.forEach(div => {
-        div.addEventListener('click', function() {
+        div.addEventListener('click', () => {
             let id = div.id;
             if (images[id]) {
                 header.style.backgroundImage = images[id];
@@ -37,6 +43,7 @@ if (header) {
             }
         });
     });
+    // si il n'y en a pas de header
 } else {
     console.log("Le header n'a pas été trouvé !");
 }
